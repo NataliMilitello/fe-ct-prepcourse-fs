963e293b0dffad29be43fb7e4f6b6ca27a845791
NataliMilitello <natalimilitello@gmail.com>
2024-02-06 22:19:45 -0300

    tarea modulo 5 ej 10-11-33

--- M05 Fundamentos Javascript/ejercicios/11.js
+++ M05 Fundamentos Javascript/ejercicios/11.js
@@ -5,8 +5,28 @@ function esMayorDeEdad(fechaNacimiento) {
   // Devuelve true si lo es, caso contrario, retorna false.
   // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
   // Tu código:
-  
-  
+  var fechaActual = new Date();
+
+  var edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
+
+  if (edad > 18) {
+    return true;
+  } else if (edad === 18) {
+    var mesNac = fechaNacimiento.getMonth();
+    var diaNac = fechaNacimiento.getDate();
+    var mesActual = fechaActual.getMonth();
+    var diaActual = fechaActual.getDate();
+
+    if (mesActual > mesNac) {
+      return true;
+    } else if (mesActual === mesNac) {
+      return diaActual >= diaNac;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
 module.exports = esMayorDeEdad;
